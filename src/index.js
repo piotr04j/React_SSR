ddeb93f5e8b744098d7d282f6d414f320bceb84d
piotr04j <darricck04@gmail.com>
2022-08-27 13:27:36 +0200

    create redux setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import 'babel-polyfill'
 import express from 'express'
 import React from 'react'
-import renderer from './helpers/render'
+import renderer from './helpers/renderer'
+import createServerStore from './helpers/createStore'
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
+  const store = createServerStore()
 
-  res.send(renderer(req))
+  res.send(renderer(req, store))
 })
 
 app.listen(3000, () => {
