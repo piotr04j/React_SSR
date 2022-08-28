3ad0c58beaf290877b128acc36afe636af5f1d89
piotr04j <darricck04@gmail.com>
2022-08-28 17:34:10 +0200

    prepared default component and adjusted routes

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,17 +1,24 @@
 import React from 'react'
+import App from './App'
 import * as Home from './pages/HomePage'
 import * as UsersList  from './pages/UsersListPage'
 
 const routes = [
   {
-    path: '/',
-    component: Home.default,
-    exact: true
-  },
-  {
-    path: '/users',
-    component: UsersList.default,
-    loadData: UsersList.loadData
+    component: App,
+    routes: [
+      {
+        path: '/',
+        component: Home.default,
+        exact: true
+      },
+      {
+        path: '/users',
+        component: UsersList.default,
+        loadData: UsersList.loadData
+      }
+
+    ]
   }
 ]
 
